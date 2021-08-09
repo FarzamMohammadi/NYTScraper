15f8711bd3aa9f506c8789bf50477e6e68c555a5
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2021-08-08 21:27:31 -0400

    All daily article links are now retrievable
    Now using selenium instead of requests in order to make sure page fully loads before scrape

--- scaper.py
+++ scaper.py
@@ -1,9 +1,46 @@
 from bs4 import BeautifulSoup
-import requests
+from selenium import webdriver
 
-todaysPaperURL = 'https://www.nytimes.com/section/todayspaper'
-htmlText = requests.get(todaysPaperURL).text
-soup = BeautifulSoup(htmlText, 'lxml')
-story = soup.findAll('p', class_='css-axufdj evys1bk0')
-for paragraphs in story:
-    print(paragraphs.text)
+
+def getTodaysHTML():
+    driver = webdriver.Chrome(executable_path="C:\Program Files (x86)\chromedriver.exe")
+    driver.get("https://www.nytimes.com/section/todayspaper")
+    htmlText = driver.page_source
+    driver.quit()
+    return htmlText
+
+
+def getArticleLinks():
+    htmlText = getTodaysHTML()
+    soup = BeautifulSoup(htmlText, 'lxml')
+    highlightArticles = soup.findAll('h2', class_='css-byk1jx e4e4i5l1')
+    rawHTMLLinks = []
+    articleLinks = []
+
+    # Retrieve highlight article links
+    for articles in highlightArticles:
+        rawHTMLLinks.append(str(articles.a))
+    for link in rawHTMLLinks:
+        beginning = link.find('/')
+        end = link.find('">')
+        articleLinks.append('https://www.nytimes.com' + link[beginning:end])
+
+    # Retrieve remaining article links
+    remainingArticles = soup.findAll('div', class_='css-141drxa')
+    for articles in remainingArticles:
+        rawHTMLLinks.append(str(articles.a))
+
+    for element in rawHTMLLinks:
+        beginning = element.find('/')
+        end = element.find('">')
+        formattedLink = element[beginning:end]
+        # Prevent correction links to be added to the list
+        if not formattedLink[12:23] == 'pageoneplus':
+            articleLinks.append('https://www.nytimes.com' + element[beginning:end])
+
+    return articleLinks
+
+
+todaysArticleLinks = getArticleLinks()
+print(todaysArticleLinks)
+print(len(todaysArticleLinks))
